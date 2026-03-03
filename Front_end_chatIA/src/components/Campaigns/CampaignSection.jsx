1caca8b2fbbd8dc92c2a194b3eb83f1e593b6dff
Geovanni Guerrero <usis015221@ugb.edu.sv.com>
2026-03-03 10:55:17 -0600

    cmabiooos2

--- Front_end_chatIA/src/components/Campaigns/CampaignSection.jsx
+++ Front_end_chatIA/src/components/Campaigns/CampaignSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Cards from '../Cards/cards.jsx';
+import Cards from '../Cards/Cards.jsx';
 import { FolderClosed } from 'lucide-react';
 import '../../layouts/ViewCampaignsMarketing/ViewCampaignsMarketing.css'; // Inherit styles
 
--- Front_end_chatIA/src/main.jsx
+++ Front_end_chatIA/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './colors.css';
 import App from './App.jsx'
-// import ViewCampaignsMarketing from './layouts/viewCampaignsMarketing/viewCampaignsMarketing.jsx';
+// import ViewCampaignsMarketing from './layouts/ViewCampaignsMarketing/ViewCampaignsMarketing.jsx';
 // import './fonts.css';
 
 createRoot(document.getElementById('root')).render(
